c49b99955e71387e51b440949ef7e0127feaa3ab
Noel8008 <nsingh@brooklynsteamcenter.org>
2024-12-12 10:28:00 -0500

    12/12

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import pymysql
 from dynaconf import Dynaconf
 
@@ -27,13 +27,37 @@ def index():
 
 @app.route("/browse")
 def product_browse(): 
+    query = request.args.get('query')
+    
     conn = connect_db()
 
     cursor = conn.cursor()
 
-    cursor.execute("SELECT * FROM `Product`;")
-
+    if query is None:
+        cursor.execute("SELECT * FROM `Product`;")
+    else:
+        cursor.execute(f"SELECT * FROM `Product` WHERE `name` LIKE '%{query}%' ;")
+    
     results = cursor.fetchall()
 
+    cursor.close()
+    conn.close()
 
     return render_template("browse.html.jinja", products = results)
+
+
+@app.route("/product/<product_id>")
+def product_page(product_id):
+   
+    conn = connect_db()
+
+    cursor = conn.cursor()
+
+    cursor.execute(f"SELECT * FROM `Product` WHERE `id` = {product_id};")
+
+    result = cursor.fetchone()
+
+    cursor.close()
+    conn.close()
+
+    return render_template("product.html.jinja", product = result)
